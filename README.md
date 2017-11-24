8d2d13f59a10fad4cad7e9912b16b48cbd9fcfb9
Chinmaya Kinnarkar <kinnarkarchinmaya@gmail.com>
2017-11-24 16:58:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Testers
+This is a live test taking web app.
+
+# Website link: https://rocky-hollows-68289.herokuapp.com/ 
+
+# Features
+1. A view for the user.
+- This view contains a view where user can register or signup.
+- The user can take a test where they get four options and have to submit one as an answer.
+- The test has a time limit and test will get automatically submitted when time's up.
+- The user can see the test scores after submitting the test.
+- The user can see the average scores scored by him/her.
+
+2. A view for the admin
+- Here admin can login and see all test taken by the user.
+- The admin can create,update,delete a test.
+- Admin can create,update,delete questions for a test.
+- Admin can get all the user data like the test taken,usernames etc.
+
+# Prerequisite
+- Git
+- NodeJs
+- Npm
+- MongoDB
+
+# Running the project
+1. Download the zip file and unzip it.
+2. Open the server folder of the project in cmd
+3. Type `npm install` to install all the dependencies
+4. Type node app.js . The project starts running on localhost:3000
+
+# Technologies used:
+# Frontend
+HTML 5 , CSS3 , Bootstrap , AngularJS
+
+# Backend
+NodeJs , ExpressJs , MongoDB , JSONWebToken , SocketIO
+
+# API tool
+Postman
+
+# OS used: 
+Windows 10
+
+# Editor used:
+Sublime Text
---
+++ app.js
@@ -0,0 +1,117 @@
+var express = require('express');
+var app =express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var methodOverride = require('method-override');
+var path = require('path');
+var fs = require('fs');
+var cors = require('cors');
+var logger = require('morgan');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+app.use(cors({
+    origin: '*',
+    withCredentials: false,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin' ]
+}));
+
+//body parser and cookie parser middleware
+app.use(bodyParser.json({limit:'10mb',extended:true}));
+app.use(bodyParser.urlencoded({limit:'10mb',extended:true}));
+app.use(cookieParser());
+
+//including passport module from other file
+require('./server/config/passport')(passport);
+
+app.use(passport.initialize()); //to initialize passport service
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+//public folder as static
+app.use(express.static(path.resolve(__dirname,'public')));
+
+//response generating Liberary
+var resGenerator = require('./server/libs/resGenerator');
+
+//Including User model and UserAuth Model
+var userModel = require('./server/app/models/User');
+
+//including controller files
+var Routes = require('./server/app/controllers/routes');
+app.use('/users', Routes);
+
+var TestRoutes = require('./server/app/controllers/testRoutes');
+app.use('/tests', TestRoutes);
+
+require('./server/app/controllers/facebookRoute')(app, passport);
+
+require('./server/app/controllers/googleRoute')(app, passport);
+
+
+var UserAuth = require('./server/app/models/UserAuth');
+var GoogleAuth = require('./server/app/models/GoogleAuth');
+
+//User Information of facebook
+
+app.get('/getUserinfofacebook', function(req, res) {
+  UserAuth.find(function(error, user) {
+    if (error) {
+      var err = resGenerator.generate(true, "Something is not working, error  : " + error, 500, null);
+      res.json(err);
+    } 
+     else {
+         var response = resGenerator.generate(false, "Successfully Got all user info", 200, user);
+          res.json(user);
+        }
+      });
+    });
+
+
+//User Information of google
+
+app.get('/getUserinfogoogle', function(req, res) {
+  GoogleAuth.find(function(error, user) {
+    if (error) {
+      var err = resGenerator.generate(true, "Something is not working, error  : " + error, 500, null);
+      res.json(err);
+    } 
+     else {
+         var response = resGenerator.generate(false, "Successfully Got all user info", 200, user);
+          res.json(user);
+        }
+      });
+    });
+
+//Setting port to 3000..
+var port = 3000;
+
+//To log HTTP Requests..
+app.use(logger('dev'));
+
+//Data Base Connection
+var dbPath = "mongodb://localhost/TestForAdmin";
+mongoose.connect(dbPath,{useMongoClient: true});
+mongoose.connection.once('open',function(){
+  console.log("Database Connection Established Successfully");
+});
+
+
+//handling 404 error.
+app.use(function(req, res, next){
+  res.status(404);
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+  res.send('Not found');
+});
+
+//Listening on port 3000
+app.listen(port,  function(){
+  console.log("Testers is Running on port:" +port);
+});
